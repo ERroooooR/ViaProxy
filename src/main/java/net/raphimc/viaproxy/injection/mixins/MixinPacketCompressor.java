bc5d943d66382f801b4016c42ba2f08a9b5134b2
ERroooooR <OoooorangeClover@intloc.cn>
2024-06-07 10:28:51 +0800

    moded fix

---
+++ src/main/java/net/raphimc/viaproxy/injection/mixins/MixinPacketCompressor.java
@@ -0,0 +1,30 @@
+/*
+ * This file is part of ViaProxy - https://github.com/RaphiMC/ViaProxy
+ * Copyright (C) 2021-2024 RK_01/RaphiMC and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.raphimc.viaproxy.injection.mixins;
+import net.raphimc.netminecraft.netty.codec.PacketCompressor;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(value = PacketCompressor.class)
+public class MixinPacketCompressor {
+    @ModifyConstant(method = "decode", constant = @Constant(intValue = 2097152))
+    private int newSize(int value) {
+        return Integer.parseInt(String.valueOf(8388608 * 100));
+    }
+}
